82bad68c4218039fedbff5d2bea4b056bfc092b3
VivekChatterjee <vivuchat84@gmail.com>
2022-04-21 13:44:32 +0530

    First Commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note()
+{
+    return (<div className="note">
+        <h1>
+        Vivek
+        </h1>
+        <p>
+            He is a Cool Guy.
+        </p>
+    </div>);
+}
+
+export default Note;
